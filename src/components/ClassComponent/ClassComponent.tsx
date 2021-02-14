a77c4242173934b90c5cf72353f1cd3c1e027b48
Marcus <marcus.i.t@outlook.com>
2021-02-14 05:35:48 -0300

    console log to components life cicle

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -2,12 +2,28 @@ import React from 'react'
 
 class ClassComponent extends React.Component<{ name: string }> {
 
+    constructor(props: any){
+        super(props)
+        console.log('Constructor reached (chegou no construtor)')
+    }
+
     state = {
         name: 'Mundo !!!'
     }
 
+    componentDidMount(){
+        console.log('componentDidMount reached (chegou no componentDidMount')
+    }
+
+    componentDidUpdate(){
+        console.log('componentDidUpdate reached (chegou no componentDidUpdate')
+    }
+
     render() {
        /* return <div>Olá {this.props.name}. Eu sou um compornente baseado em classe</div>*/
+
+       console.log('Render reached (chegou no Render)')
+
        return <div>
            <p>name : { this.state.name }</p>
            <button onClick={ () => {
