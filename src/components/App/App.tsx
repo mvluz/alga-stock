b84b37f5e2fc1a98b49f7333c9e24e13941822dd
Marcus <marcus.i.t@outlook.com>
2021-02-14 06:03:12 -0300

    first useState

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <TestComponent/>
+          <TestComponent name="Marcus"/>
           <ClassComponent name= 'Mundo'/>
         </p>
         <a
--- src/components/TestComponent/TesteComponent.tsx
+++ src/components/TestComponent/TesteComponent.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TestComponent.css'
 
+/* Arrow function
 const TestComponent = () => (
     <div className="TestComponent">TestComponent</div>
 )
+*/
+
+/*
+function TestComponent (props: {name: string})  {
+    return <div className="TestComponent">
+        Olá, {props.name}
+    </div>
+}
+*/
+
+function TestComponent (props: {name: string})  {
+    /*
+    const state = {
+        age: 21
+    }
+    */
+    const [age,setAge] = useState(32)
+
+                             /*{state.age}*/
+    return <div className="TestComponent">
+        Olá, {props.name}, { age }
+        <button onClick={() => {
+            setAge(age + 1)
+            //state.age++
+            //console.log(state.age)
+        }}>
+            +
+        </button>
+    </div>
+}
+
 export default TestComponent
