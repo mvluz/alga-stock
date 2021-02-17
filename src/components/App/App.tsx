053d0b77bec5a211387a73b11ea3efe51f4b9253
mvluz <marcus.i.t@outlook.com>
2021-02-16 22:49:22 -0300

    add style table

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,11 +4,6 @@ import './App.css';
 import Container from '../Shared/Container';
 import Table from '../Shared/Table';
 
-function TestComponent () {
-  return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" 
-  alt="Seach icon"/>
-}
-
 function App() {
   return (
     <div className="App">
