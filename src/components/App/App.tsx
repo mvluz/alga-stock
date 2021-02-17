725914ff12862a3f70c3273b312334fa18d25074
mvluz <marcus.i.t@outlook.com>
2021-02-16 22:31:09 -0300

    add table html structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Header';
 import './App.css';
 import Container from '../Shared/Container';
+import Table from '../Shared/Table';
 
 function TestComponent () {
   return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" 
@@ -14,7 +15,7 @@ function App() {
       <Header title="Alga-Stock"/>
       
       <Container>
-
+        <Table />
       </Container>
     </div>
   );
