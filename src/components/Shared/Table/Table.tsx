261e4d7b89f4dd8d84786133d159b3d854d9e8c4
mvluz <marcus.i.t@outlook.com>
2021-02-16 23:13:11 -0300

    add table mock data

--- src/components/Shared/Table/Table.tsx
+++ src/components/Shared/Table/Table.tsx
@@ -1,26 +1,31 @@
 import React from 'react'
 import './Table.scss'
+import fakeProducts from './Table.mockdata'
+
+const fakeCabecalho = [
+    { key: 'name', value: 'Product'},
+    { key: 'price', value: 'Price'},
+    { key: 'actions', value: 'Actions' },
+    { key: 'stock', value: 'Available Stock'},
+]
 
 const Table = () => {
     return <table className="AppTable">
     <thead>
       <tr>
-        <th>Product</th>
-        <th>Price</th>
-        <th className="right">Stock</th>
+          {
+              fakeCabecalho.map(intera => <th key={intera.key}>{intera.value}</th>)
+          }
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>Cookie</td>
-        <td>$1.25</td>
-        <td className="right">23</td>
-      </tr>
-      <tr>
-        <td>Milk 1L</td>
-        <td>$0.99</td>
-        <td className="right">10</td>
-      </tr>
+        {
+            fakeProducts.map(intera => <tr>
+                <td>{ intera.name }</td>
+                <td>${intera.price}</td>
+                <td className="right">{intera.stock}</td>
+            </tr>)
+        }
     </tbody>    
     </table>
 }
