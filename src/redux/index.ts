5d90d6f6328f6ecc48e5866310ea0e31548b3cff
mvluz <marcus.i.t@outlook.com>
2023-03-05 00:52:26 -0300

    add redux devtools

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -5,6 +5,10 @@ const reducers = combineReducers({
     products : Products
 })
 
-const store = legacy_createStore(reducers)
+const store = legacy_createStore(
+    reducers,
+    //@ts-ignore
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 
 export default store
